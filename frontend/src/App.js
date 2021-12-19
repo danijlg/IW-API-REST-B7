ce27b9a6fc2dcd0fea6f7931490e232c3ab732a0
franbono2 <franbono2@gmail.com>
2021-12-19 20:14:33 +0100

    PUT de comentario y usuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,8 @@ import Map from './Map.js';
 
 
 function Api_Django(){
-<<<<<<< HEAD
-    const [UsuarioList, getUsuarioList]=useState([]);
-=======
     // GET
     const [UsuarioList,getUsuarioList]=useState([]);
->>>>>>> origin/master
     const [CommentsList, getCommentsList]=useState([]);
     const [usuarioById, getUsuarioById]=useState([]);
     const [comentarioById, getComentarioById]=useState([]);
@@ -42,6 +38,23 @@ function Api_Django(){
         GetUsuarioList()
     }
 
+    function PutUsuario(id){
+        var name = document.getElementById("nameUserPut").value;
+        var surname = document.getElementById("surnameUserPut").value;
+        var address = document.getElementById("addressUserPut").value;
+        if(name == "" || surname == "" || address == "") alert("Rellena todos los campos");
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: name, surname: surname, address: address })
+        };
+        fetch('http://127.0.0.1:8000/crud/usuario/'+ id  + '/', requestOptions)
+            .then(response => response.json())
+            .then(error=>console.log(error))
+        alert("Usuario actualizado correctamente")
+        GetUsuarioList()
+    }
+
     function GetCommentsList(){
         fetch('http://127.0.0.1:8000/crud/comentario/',
         {
@@ -64,6 +77,23 @@ function Api_Django(){
         }).then(response=>response.json())
         .then(error=>console.log(error))
         alert("Comentario borrado con exito")
+        GetCommentsList()
+    }
+
+    function PutComentario(id){
+        var author = document.getElementById("authorCommentPut").value;
+        var comment = document.getElementById("commentCommentPut").value;
+        var date = document.getElementById("dateCommentPut").value;
+        if(author == "" || comment == "" || date == "") alert("Rellena todos los campos");
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ author: author, coment: comment, date: date })
+        };
+        fetch('http://127.0.0.1:8000/crud/comentario/' + id + '/', requestOptions)
+            .then(response => response.json())
+            .then(error=>console.log(error))
+        alert("Comentario actualizado correctamente")
         GetCommentsList()
     }
 
@@ -434,6 +464,38 @@ function Api_Django(){
                         <button onClick={PostComentario}>
                             Insertar Comentario
                         </button>
+
+                        <h3>Actualizar Usuario por ID</h3>
+                        <input type="number" placeholder="Escriba una id válida" onChange={(evt) => {GetUsuarioById(evt.target.value);}}></input>
+                        <br/>
+                        
+                        <label><br/>ID<br/></label>
+                        <input type="number" value={usuarioById.id} id='idUser' disabled></input>
+                        <label><br/>Nombre<br/></label>
+                        <input type="text" value={usuarioById.name} id='nameUserPut'></input>
+                        <label><br/>Apellido<br/></label>
+                        <input type="text" value={usuarioById.surname} id='surnameUserPut'></input>
+                        <label><br/>Direccion<br/></label>
+                        <input type="text" value={usuarioById.address} id='addressUserPut'></input>
+                        <br/>
+                        <br/>
+                        <button type="submit" onClick={() => PutUsuario(document.getElementById("idUser").value)}> Actualizar Usuario </button>
+
+                        <h3>Actualizar Comentario por ID</h3>
+                        <input type="number" placeholder="Escriba una id válida" onChange={(evt) => {GetComentarioById(evt.target.value);}}></input>
+                        <br/>
+                        
+                        <label><br/>ID<br/></label>
+                        <input type="number" value={comentarioById.id} id='idComment' disabled ></input>
+                        <label><br/>Autor<br/></label>
+                        <input type="number" value={comentarioById.author} id='authorCommentPut'></input>
+                        <label><br/>Comentario<br/></label>
+                        <input type="text" value={comentarioById.coment} id='commentCommentPut'></input>
+                        <label><br/>Fecha<br/></label>
+                        <input type="date" value={comentarioById.date}  id='dateCommentPut'></input>
+                        <br/>
+                        <br/>
+                        <button type="submit" onClick={() => PutComentario(document.getElementById("idComment").value)}>  Actualizar Comentario </button>
                 </div>
             </body>
         </html>
